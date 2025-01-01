4d0467f8b8953ba129bc70ea6d9bb9faf8131cfc
Cornelis Terblanche <corry@nibbleabit.com>
2025-01-02 01:57:53 +0200

    chore: update path alias configuration - Move path aliases to SvelteKit config - Remove path aliases from tsconfig.json - Fix SvelteKit warning

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$test: 'src/test',
-			$lib: 'src/lib',
-			$routes: 'src/routes'
-		}
-	},
-	preprocess: vitePreprocess()
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $lib: "./src/lib",
+      $tests: "./src/tests",
+      $env: "./src/env",
+    },
+  },
+  preprocess: vitePreprocess(),
 };
 
 export default config;
