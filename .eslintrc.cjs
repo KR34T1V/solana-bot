b9a6084bf0834165024de0ac7867e7748074d870
Cornelis Terblanche <corry@nibbleabit.com>
2025-01-02 01:01:42 +0200

    chore: setup linting infrastructure
    - Added ESLint configuration with TypeScript and Svelte support
    
    - Added GitHub Actions workflow for linting
    
    - Added pre-commit hooks with husky
    
    - Updated package.json with lint scripts

---
+++ .eslintrc.cjs
@@ -0,0 +1,52 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['*.cjs'],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte']
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  },
+  rules: {
+    // TypeScript specific rules
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports' }
+    ],
+    
+    // General rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'eqeqeq': ['error', 'always'],
+    
+    // Svelte specific rules
+    'svelte/no-at-html-tags': 'error',
+    'svelte/require-store-callbacks-use-set-param': 'error',
+    'svelte/require-store-reactive-access': 'error'
+  }
+}; 
